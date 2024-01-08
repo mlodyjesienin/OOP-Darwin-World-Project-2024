9567d42042c3f540b70a02dbaa70c80ebe6a1358
Mpus <mpodmokly@student.agh.edu.pl>
2024-01-08 14:28:57 +0100

    ui basics

---
+++ source/src/main/java/agh/ui/SimulationApp.java
@@ -0,0 +1,10 @@
+package agh.ui;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class SimulationApp extends Application {
+    @Override
+    public void start(Stage primaryStage){
+        primaryStage.show();
+    }
+}
---
+++ source/src/main/java/agh/ui/WorldGUI.java
@@ -0,0 +1,9 @@
+package agh.ui;
+
+import javafx.application.Application;
+
+public class WorldGUI {
+    public static void main(String [] args){
+        Application.launch(SimulationApp.class, args);
+    }
+}
