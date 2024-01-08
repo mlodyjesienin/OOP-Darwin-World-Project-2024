ee08b564551088ebbfe70c44e88a094a7b73d5bd
Mpus <mpodmokly@student.agh.edu.pl>
2024-01-08 13:49:34 +0100

    compile

--- source/src/main/java/agh/World.java
+++ source/src/main/java/agh/World.java
@@ -5,13 +5,10 @@ import java.util.*;
 import static java.lang.Math.floor;
 
 public class World {
-
     public static void main(String[] args) {
         Simulation simulation = new Simulation(0, 0, 5, 5, 5, 2,
                 10, 36, 3, 3, 3, 3, 4, 5, 15);
 
         simulation.run();
-
-
     }
 }
