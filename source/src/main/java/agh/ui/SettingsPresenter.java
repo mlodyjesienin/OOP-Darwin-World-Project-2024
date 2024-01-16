6ff9dfd3b0ff80be56639116832258dce04f2673
Mpus <mpodmokly@student.agh.edu.pl>
2024-01-16 17:23:40 +0100

    map size input & check

--- source/src/main/java/agh/ui/SettingsPresenter.java
+++ source/src/main/java/agh/ui/SettingsPresenter.java
@@ -15,43 +15,88 @@ public class SettingsPresenter {
     @FXML
     private TextField mapWidth;
     @FXML
-    private Label badValue;
+    private TextField mapHeight;
+    @FXML
+    private Label badWValue;
+    @FXML
+    private Label badHValue;
 
     @FXML
     private void onSimulationStartClicked() throws IOException {
-        String mapWidthValue = mapWidth.getText();
+        boolean canStart = true;
+        int mapWidthVal = 0;
+        int mapHeightVal = 0;
+        String mapWidthText = mapWidth.getText();
 
         try {
-            int mapWidth = Integer.parseInt(mapWidthValue);
+            mapWidthVal = Integer.parseInt(mapWidthText);
+            badWValue.setVisible(false);
 
-            if (mapWidth > 0){
-                if (mapWidth < 2000){
-                    badValue.setVisible(false);
+            if (mapWidthVal > 0){
+                badWValue.setVisible(false);
 
-                    FXMLLoader loader = new FXMLLoader();
-                    loader.setLocation(getClass().getClassLoader().getResource("simulation.fxml"));
-                    VBox viewRoot = loader.load();
-                    Stage stage = new Stage();
-                    configureStage(stage, viewRoot);
-                    stage.show();
-
-                    Simulation simulation = new Simulation(0, 0, 5, 5, 5, 2, 10, 36, 3, 3, 3, 3, 4, 5, 15);
-                    simulation.registerPresenter(loader.getController());
-                    engine.runAsyncInThreadPool(simulation);
+                if (mapWidthVal <= 2000){
+                    badWValue.setVisible(false);
                 }
                 else {
-                    badValue.setText("map width too large!");
-                    badValue.setVisible(true);
+                    badWValue.setText("map width too large!");
+                    badWValue.setVisible(true);
+                    canStart = false;
                 }
             }
             else {
-                badValue.setText("map width value cannot be lower than 0!");
-                badValue.setVisible(true);
+                badWValue.setText("map width value cannot be lower than 0!");
+                badWValue.setVisible(true);
+                canStart = false;
             }
         }
         catch (NumberFormatException e){
-            badValue.setText("bad map width value!");
-            badValue.setVisible(true);
+            badWValue.setText("bad map width value!");
+            badWValue.setVisible(true);
+            canStart = false;
+        }
+
+        String mapHeightValue = mapHeight.getText();
+
+        try {
+            mapHeightVal = Integer.parseInt(mapHeightValue);
+            badHValue.setVisible(false);
+
+            if (mapHeightVal > 0){
+                badHValue.setVisible(false);
+
+                if (mapHeightVal <= 2000){
+                    badHValue.setVisible(false);
+                }
+                else {
+                    badHValue.setText("map height too large!");
+                    badHValue.setVisible(true);
+                    canStart = false;
+                }
+            }
+            else {
+                badHValue.setText("map height value cannot be lower than 0!");
+                badHValue.setVisible(true);
+                canStart = false;
+            }
+        }
+        catch (NumberFormatException e) {
+            badHValue.setText("bad map height value!");
+            badHValue.setVisible(true);
+            canStart = false;
+        }
+
+        if (canStart){
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getClassLoader().getResource("simulation.fxml"));
+            VBox viewRoot = loader.load();
+            Stage stage = new Stage();
+            configureStage(stage, viewRoot);
+            stage.show();
+
+            Simulation simulation = new Simulation(0, 0, mapHeightVal, mapWidthVal, 5, 2, 10, 36, 3, 3, 3, 3, 4, 5, 15);
+            simulation.registerPresenter(loader.getController());
+            engine.runAsyncInThreadPool(simulation);
         }
     }
 
